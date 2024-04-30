2e966cd6a2a1773a3f501ca5b3b01bba542b0181
tot-ger <gergoototh@gmail.com>
2024-04-30 15:47:08 +0800

    Add GomokuHub class to handle SignalR communication for Gomoku game

--- api/Program.cs
+++ api/Program.cs
@@ -1,5 +1,27 @@
+using GameHub.API;
+using GameHub.GomokuEngine;
+using GameHub.GomokuEngine.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGomokuGameEngine();
+builder.Services.AddSignalR();
+
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseCors(builder => builder
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .SetIsOriginAllowed(origin => true) // allow any origin
+        .AllowCredentials());
+}
+
+app.MapHub<GomokuHub>("/gomokuHub");
 
 app.MapGet("/", () => "Hello World!");
 

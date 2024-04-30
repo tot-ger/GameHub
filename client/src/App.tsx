099040e1c90fdc8a1e67c25931bb0edfdfc8c856
tot-ger <gergoototh@gmail.com>
2024-04-30 16:13:21 +0800

    Refactor App.tsx to add routes for Home, Lobby, and Game components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,34 @@
+import { useState, useEffect, useRef } from "react";
+import { Routes, Route, useNavigate, useParams } from "react-router-dom";
+
+type GameInfo = {
+  id: string;
+  player: string;
+}
 
 function App() {
-
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="lobby" element={<Lobby />} />
+      <Route path="game/:id" element={<Game />} />
+    </Routes>
   )
+}
+
+function Home() {
+  return(<></>)
+}
+
+function Lobby() {
+  return (
+    <></>
+  )
+}
+
+function Game() {
+  const { id } = useParams();
+  return (<></>)
 }
 
 export default App
